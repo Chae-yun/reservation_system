79c49a7abcbdb5a8d9cf7c9dd30117bc466cc383
Chaeyun Yang <ycy6685@naver.com>
2016-05-10 20:55:40 +0900

    #3 Third Event Programming

--- app/src/main/java/kr/hs/emirim/ycy6685/reservationsystem/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/ycy6685/reservationsystem/MainActivity.java
@@ -1,5 +1,7 @@
 package kr.hs.emirim.ycy6685.reservationsystem;
 
+import android.graphics.Color;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +11,8 @@ import android.widget.Chronometer;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -49,6 +53,35 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 calView.setVisibility(View.INVISIBLE);
                 timePicker.setVisibility(View.VISIBLE);
+            }
+        });
+
+        butStart.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                chrono.setBase(SystemClock.elapsedRealtime()); //long 타입
+                chrono.start();
+                chrono.setTextColor(Color.RED);
+            }
+        });
+
+        butDone.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                chrono.stop();
+                chrono.setTextColor(Color.BLUE);
+                Calendar cal=Calendar.getInstance(); //추상클래스라 뉴 못씀
+                cal.setTimeInMillis(calView.getDate()); //시스템 날짜로 하려면 이문장 ㄴㄴ 우리는 캘린더 객체의 날짜로 하기 위해서
+                //getDate가 밀리세컨드로 반환해서 setTimeInMillis 쓴거임?
+
+                int year=cal.get(Calendar.YEAR); //캘린더뷰에 설정된 연도 반환 가능, 상수 필드로!
+                int month=cal.get(Calendar.MONTH)+1; //달은 0부터 시작해서 나와서 +1
+                int date=cal.get(Calendar.DATE);
+                //int hour=timePicker.getHour(); //23버전 이하는 이게 안 되는데 그러면 캘린더처럼 해야 됨
+                //int minute=timePicker.getMinute();
+
+                String dateAndTime=year+"년 "+month+"월 "+date+"일";
+                textResult.setText(dateAndTime);
             }
         });
     }
